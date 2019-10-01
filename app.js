bc935c7229558c2eda41e279b5ac529764201c73
Henrique Dolzane <henriquedolzane@gmail.com>
2019-09-30 21:58:51 -0400

    adicionado novo layout com flexbox e integrado editor de codigo fonte e modulo de renderização com iframe

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser')
+const json2html = require('html2json').json2html;
 const arq_caminhos = require('./model/util/arquivo_caminhos');
 
 dotenv.config();
@@ -22,10 +23,16 @@ app.use(express.static('public'));
 //porta do servidor
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.render('index',arq_caminhos.caminhos)
+app.get('/', (req, res) => {
+    res.render('index', arq_caminhos.caminhos)
 });
 
-app.listen(port,()=>{
+app.post('/renderizar', (req, res) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.send(req.body.dados);
+});
+
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
